0c181da47c51e4204fe7a053c697311549142113
Tomasz Aniolowski <aniolowski93@gmail.com>
2019-07-10 22:32:20 +0200

    create structure of the project

---
+++ src/main/java/com/musicsite/app/AppConfig.java
@@ -0,0 +1,55 @@
+package com.musicsite.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.musicsite")
+@EnableTransactionManagement
+public class AppConfig implements WebMvcConfigurer {
+
+    // === HIBERNATE CONFIGURATIONS
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("musicsitePersistenceUnit");
+        return emfb;
+    }
+
+    // === HIBERNATE CONFIGURATIONS
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager tm = new JpaTransactionManager(emf);
+        return tm;
+    }
+
+    // === VIEWRESOLVER
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver =
+                new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    // === VIEWRESOLVER: ENABLE OTHER FILES
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
