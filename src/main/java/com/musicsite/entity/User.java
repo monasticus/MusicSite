4341943004008549be7a3c5cbfd9a4f26a251511
Tomasz Aniolowski <aniolowski93@gmail.com>
2019-07-12 19:35:24 +0200

    create User entity

--- src/main/java/com/musicsite/entity/User.java
+++ src/main/java/com/musicsite/entity/User.java
@@ -1,4 +1,85 @@
 package com.musicsite.entity;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 8, max = 50)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    @Override
+    public String toString() {
+        return username + "(email: " + email + ")";
+    }
 }
