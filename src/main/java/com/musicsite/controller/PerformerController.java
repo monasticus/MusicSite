b41ee0afeed6a6b55be45c35440fef611f6d5cc1
Tomasz Aniolowski <aniolowski93@gmail.com>
2019-07-14 16:11:56 +0200

    Performer view start

--- src/main/java/com/musicsite/controller/PerformerController.java
+++ src/main/java/com/musicsite/controller/PerformerController.java
@@ -2,6 +2,7 @@ package com.musicsite.controller;
 
 import com.musicsite.entity.Album;
 import com.musicsite.entity.Performer;
+import com.musicsite.entity.Track;
 import com.musicsite.repository.AlbumRepository;
 import com.musicsite.repository.CategoryRepository;
 import com.musicsite.repository.PerformerRepository;
@@ -10,24 +11,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/performer")
 public class PerformerController {
 
     private PerformerRepository performerRepository;
-    private TrackRepository trackRepository;
-    private AlbumRepository albumRepository;
-    private CategoryRepository categoryRepository;
 
     @Autowired
-    public PerformerController(PerformerRepository performerRepository, TrackRepository trackRepository, AlbumRepository albumRepository, CategoryRepository categoryRepository) {
+    public PerformerController(PerformerRepository performerRepository) {
         this.performerRepository = performerRepository;
-        this.trackRepository = trackRepository;
-        this.albumRepository = albumRepository;
-        this.categoryRepository = categoryRepository;
+    }
+
+    @ModelAttribute("performers")
+    public List<Performer> getPerformers() {
+        return performerRepository.findAll();
     }
 
     @GetMapping("/{performerPseudonym}")
@@ -38,7 +41,7 @@ public class PerformerController {
 
         model.addAttribute("performer", performer);
 
-        return "performer";
+        return "main/performer";
     }
 
 }
--- src/main/webapp/WEB-INF/views/main/performer.jsp
+++ src/main/webapp/WEB-INF/views/main/performer.jsp
@@ -3,14 +3,46 @@
 <html>
 <head>
     <title>musicsite - ${performer.pseudonym}</title>
-    <%@include file="../fragments/head.html"%>
+    <%@include file="../fragments/head.html" %>
     <style>
         <%@include file="../css/style.css"%>
     </style>
 </head>
 <body>
 <%@include file="../fragments/header.jspf" %>
-
 <%@include file="../fragments/dashboard.jspf" %>
+
+<section class="ens-page">
+    <div class="ens-top">
+        <div class="ens-image">
+            <i class="glyphicon glyphicon-user"></i>
+        </div>
+    </div>
+    <div class="ens-basic-information">
+        <div class="ens-name">
+            ${performer.pseudonym}
+        </div>
+    </div>
+    <div class="ens-mid">
+        <div class="related-enta">
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Albums</a>
+                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Tracks</a>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+
+
+                </div>
+                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
 </html>
